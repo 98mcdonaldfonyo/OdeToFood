7aefae5d5b664bb3890325c0c3ff0b7b38b81594
Mc Donald Fonyo <mcdonaldfonyo@outlook.com>
2020-07-14 18:46:47 +0200

    Updating the data stored in InMemory

---
+++ Pages/Resturants/Edit.cshtml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using OdeToFood.Core;
+using OdeToFood.Data;
+
+namespace OdeToFood.Pages.Resturants
+{
+    public class EditModel : PageModel
+    {
+        private readonly IResturantData resturantData;
+        private readonly IHtmlHelper htmlHelper;
+        [BindProperty]
+        public Resturant  Resturant { get; set; }
+        public IEnumerable<SelectListItem> Cusines { get; set; }
+
+
+        public EditModel(IResturantData resturantData,IHtmlHelper htmlHelper)
+        {
+            this.resturantData = resturantData;
+            this.htmlHelper =
+                htmlHelper;
+        }
+        public IActionResult OnGet(int resturantId)
+        {
+            Cusines = htmlHelper.GetEnumSelectList<CuisineType>();
+            Resturant = resturantData.getById(resturantId);
+            if (Resturant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+            return Page();
+        }
+        public IActionResult OnPost()
+        {
+            Cusines = htmlHelper.GetEnumSelectList<CuisineType>();
+            resturantData.Update(Resturant);
+            resturantData.Commit();
+
+
+            return Page();
+        }
+    }
+}
