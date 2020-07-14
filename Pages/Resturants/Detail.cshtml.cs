7f3868a24b2a1694a0bd98ff04087554b3724833
Mc Donald Fonyo <mcdonaldfonyo@outlook.com>
2020-07-14 16:16:55 +0200

    Search by ID

--- Pages/Resturants/Detail.cshtml.cs
+++ Pages/Resturants/Detail.cshtml.cs
@@ -5,17 +5,28 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using OdeToFood.Core;
+using OdeToFood.Data;
 
 namespace OdeToFood.Pages.Resturants
 {
     public class DetailModel : PageModel
     {
+        private readonly IResturantData resturantData;
+
         public Resturant Resturant { get; set; }
     
-        public void OnGet(int ResturantId)
+        public IActionResult OnGet(int ResturantId)
         {
-            Resturant = new Resturant();
-            Resturant.Id = ResturantId;
+            Resturant = resturantData.getById(ResturantId);
+            if (Resturant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+            return Page();
+        }
+        public DetailModel(IResturantData resturantData)
+        {
+            this.resturantData = resturantData;
         }
     }
 }
