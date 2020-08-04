7fd637c696779bbfd607979b5eaa85a94f6eb5b3
Mc Donald Fonyo <mcdonaldfonyo@outlook.com>
2020-08-04 19:04:57 +0200

    Delete resturant by ID

---
+++ Pages/Resturants/Delete.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToFood.Core;
+using OdeToFood.Data;
+
+namespace OdeToFood.Pages.Resturants
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly IResturantData resturantData;
+        public Resturant Resturant { get; set; }
+
+        public DeleteModel(IResturantData resturantData)
+        {
+            this.resturantData = resturantData;
+        }
+        public IActionResult OnGet(int resturantID)
+        {
+            Resturant = resturantData.getById(resturantID);
+            if (Resturant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+            return Page();
+        }
+        public IActionResult OnPost(int resturantID)
+        {
+            var resturant = resturantData.Delete(resturantID);
+            resturantData.Commit();
+            if (resturant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+            TempData["Message"] = $"{resturant.NBame}deleted";
+            return RedirectToPage("./List");
+
+        }
+    }
+}
